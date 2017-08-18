4b20d37de746da0d3d23b7cc74a6c69cb149631b
Justin Schneck <jschneck@mac.com>
2017-08-18 17:52:12 -0400

    update json encode mix task

--- lib/mix/tasks/nerves.test.env.ex
+++ lib/mix/tasks/nerves.test.env.ex
@@ -3,20 +3,20 @@ defmodule Mix.Tasks.Json.Encode do
   @moduledoc """
   JSON Encode all flags out to a file
   Example
-    mix json.encode /my/file --repo-name nerves-project
-
+    $ mix json.encode context.json --repo-name nerves-project --enabled
+    $ cat context.json
+    {"--repo-name":"nerves-project","enabled":true}
   """
 
   def run([file | argv]) do
-    {_, _, opts} = OptionParser.parse(argv)
-    json =
-      opts
-      |> Enum.into(%{})
-      |> Poison.encode!
+    {opts2, _, opts} = OptionParser.parse(argv) |> IO.inspect
+    opts = Enum.into(opts ++ opts2, %{})
     file = Path.expand(file)
-    case File.write(file, json) do
+    with {:ok, json} <- Poison.encode(opts),
+                 :ok <- File.write(file, json) do
+      :ok
+    else
       {:error, error} -> Mix.raise("Error writing json to file #{file} #{error}")
-      :ok -> :ok
     end
   end
 end
