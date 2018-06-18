577da8d04bace8056d51de6a47968ab8478a6ec7
Frank Hunleth <fhunleth@troodon-software.com>
2018-06-18 14:22:20 -0400

    Format!

--- lib/mix/tasks/json.encode.ex
+++ lib/mix/tasks/json.encode.ex
@@ -1,5 +1,4 @@
 defmodule Mix.Tasks.Json.Encode do
-
   @moduledoc """
   JSON Encode all flags out to a file
   Example
@@ -9,13 +8,14 @@ defmodule Mix.Tasks.Json.Encode do
   """
 
   def run([file | argv]) do
-    {opts2, _, opts} = OptionParser.parse(argv) |> IO.inspect
+    {opts2, _, opts} = OptionParser.parse(argv) |> IO.inspect()
     opts = Enum.map(opts, &strip/1)
     opts = (opts ++ opts2) |> Enum.into(%{})
 
     file = Path.expand(file)
+
     with {:ok, json} <- Jason.encode(opts),
-                 :ok <- File.write(file, json) do
+         :ok <- File.write(file, json) do
       :ok
     else
       {:error, error} -> Mix.raise("Error writing json to file #{file} #{error}")
@@ -23,9 +23,11 @@ defmodule Mix.Tasks.Json.Encode do
   end
 
   def strip({k, v}) do
-    k = k
+    k =
+      k
       |> String.trim_leading("--")
       |> String.replace("-", "_")
+
     {k, v}
   end
 end
